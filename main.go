df40109c83a617cee4dd05a92b5c46183d9d83a1
niklas@windows.local <niklas@windows.local>
2025-08-22 21:01:27 +0200

    .

--- main.go
+++ main.go
@@ -147,24 +147,19 @@ func print_main_window() {
 	}
 	menu := newPrimitive("Menu")
 	main := newPrimitive("Main content")
-	sideBar := newPrimitive("Side Bar")
+
+	button := tview.NewButton("Hit Enter to close")
+	button.SetBorder(true).SetRect(0, 0, 22, 3)
 
 	grid := tview.NewGrid().
-		SetRows(3, 0, 3).
-		SetColumns(30, 0, 30).
+		SetRows(3, 0).
+		SetColumns(30, 0).
 		SetBorders(true).
-		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
-		AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
-
-	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
-	grid.AddItem(menu, 0, 0, 0, 0, 0, 0, false).
-		AddItem(main, 1, 0, 1, 3, 0, 0, false).
-		AddItem(sideBar, 0, 0, 0, 0, 0, 0, false)
+		AddItem(newPrimitive("Header"), 0, 0, 1, 2, 0, 0, false)
 
 	// Layout for screens wider than 100 cells.
 	grid.AddItem(menu, 1, 0, 1, 1, 0, 100, false).
-		AddItem(main, 1, 1, 1, 1, 0, 100, false).
-		AddItem(sideBar, 1, 2, 1, 1, 0, 100, false)
+		AddItem(main, 1, 1, 1, 1, 0, 100, false)
 
 	if err := tview.NewApplication().SetRoot(grid, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
